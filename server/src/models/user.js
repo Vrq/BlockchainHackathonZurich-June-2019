ba290d91e34e01927b198c95ae63a3df3f03dd55
Ben Bullinger <47496939+EBNF@users.noreply.github.com>
2019-06-22 00:03:43 +0200

    Added endpoints to get user data as batch or by id

--- server/src/models/user.js
+++ server/src/models/user.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 const validator = require('validator')
 
-const User = mongoose.model('Dummy', {
+const User = mongoose.model('User', {
     name: {
         type: String,
         required: true,
