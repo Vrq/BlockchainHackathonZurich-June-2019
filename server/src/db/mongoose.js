c50a9f57841caace4b50e5acd9551b89b399ee48
Janusz Tomasik <januszveiro@gmail.com>
2019-06-21 22:36:38 +0200

    Change credentials handling

--- server/src/db/mongoose.js
+++ server/src/db/mongoose.js
@@ -1,11 +1,14 @@
-const fs = require('fs')
 const mongoose = require('mongoose')
-const loginJSON = JSON.parse(fs.readFileSync('./src/db/login.json'))
 
-const user = loginJSON[0].user
-const password = loginJSON[0].password
+const env = process.env.NODE_ENV || 'development';
+let config = {};
+if (env !== "production") {
+    config = require('./config');
+}
+const USER = process.env.DB_USER || config.DB_USER
+const PASSWORD = process.env.DB_PASS || config.DB_PASS
 
-mongoose.connect('mongodb+srv://' + user + ':' + password + '@kuznyechik-klr4c.mongodb.net/test?retryWrites=true&w=majority', {
+mongoose.connect('mongodb+srv://' + USER + ':' + PASSWORD + '@kuznyechik-klr4c.mongodb.net/test?retryWrites=true&w=majority', {
     useNewUrlParser: true,
     useCreateIndex: true,
     useFindAndModify: false
