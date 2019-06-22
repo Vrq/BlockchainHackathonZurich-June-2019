b1b27d3788280f25a600b14e8a34ce27043ddcfb
Anton Permenev <anton.permenev@gmail.com>
2019-06-22 15:18:25 +0200

    changes

--- client/src/components/pages/MetamaskPage.js
+++ client/src/components/pages/MetamaskPage.js
@@ -1,14 +1,24 @@
 import React from 'react';
+import { MDBCol, MDBRow, MDBInput, MDBBtn } from 'mdbreact';
 
 class StartPage extends React.Component {
+    constructor(props){
+        super(props)
+        this.foo = this.foo.bind(this);
+
+    }
     componentDidMount() {
         console.log("this gets called once when the component is first renered - you can do POST/GET other stuff here")
+    }
+
+    foo() {
+      console.log('click')
     }
 
     render() {
         return (
             <div>
-                Empty metamask page
+                <MDBBtn color="primary" onClick={this.foo}>Test</MDBBtn>
             </div>
         );
     }
