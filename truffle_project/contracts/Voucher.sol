10966e5187f4967d9ae0f886087dd455ef95e435
Anton Permenev <anton.permenev@gmail.com>
2019-06-22 13:15:17 +0200

    more contracts

--- truffle_project/contracts/Voucher.sol
+++ truffle_project/contracts/Voucher.sol
@@ -1,3 +1,15 @@
 pragma solidity ^0.5.0;
 
 import 'openzeppelin-solidity/contracts/token/ERC20/ERC20Mintable.sol';
+import 'openzeppelin-solidity/contracts/lifecycle/Pausable.sol';
+
+
+contract Voucher is ERC20Mintable, Pausable{
+    string public name = "Voucher";
+    bytes32 public id;
+
+    constructor(bytes32 _id) public {
+        id = _id;
+    }
+
+}
