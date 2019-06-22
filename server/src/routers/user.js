57b4abfb8aef78120b6c353655be3e09975275ee
Ben Bullinger <47496939+EBNF@users.noreply.github.com>
2019-06-22 12:01:46 +0200

    Restrict user privileges to own data

--- server/src/routers/user.js
+++ server/src/routers/user.js
@@ -51,21 +51,7 @@ router.get('/users/me', auth, async (req, res) => {
     res.send(req.user)
 })
 
-router.get('/users/:id', auth, async (req, res) => {
-    try {
-        const user = await User.findById(req.params.id)
-
-        if (!user) {
-            return res.status(404).send()
-        }
-
-        res.send(user)
-    } catch (e) {
-        res.status(500).send()
-    }
-})
-
-router.patch('/users/:id', auth, async (req, res) => {
+router.patch('/users/me', auth, async (req, res) => {
     const updates = Object.keys(req.body)
     const allowedUpdates = ['name', 'email', 'password']
     const isValidOperation = updates.every((update) => allowedUpdates.includes(update))
@@ -75,30 +61,19 @@ router.patch('/users/:id', auth, async (req, res) => {
     }
 
     try {
-        const user = await User.findById(req.params.id)
+        updates.forEach((update) => req.user[update] = req.body[update])
+        await req.user.save()
 
-        updates.forEach((update) => user[update] = req.body[update])
-        await user.save()
-
-        if (!user) {
-            return res.status(404).send()
-        }
-
-        res.send(user)
+        res.send(req.user)
     } catch (e) {
         res.status(400).send(e)
     }
 })
 
-router.delete('/users/:id', auth, async (req, res) => {
+router.delete('/users/me', auth, async (req, res) => {
     try {
-        const user = await User.findByIdAndDelete(req.params.id)
-
-        if (!user) {
-            return res.status(404).send()
-        }
-
-        res.send(user)
+        await req.user.remove()
+        res.send(req.user)
     } catch (e) {
         res.status(500).send()
     }
