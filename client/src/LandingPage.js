e8ded112b037c5f201dfe20cc9ad8bd332f58e21
Polina Shichkova <polina.shichkova@epfl.ch>
2019-06-22 13:28:16 +0200

    img landing v3

--- client/src/LandingPage.js
+++ client/src/LandingPage.js
@@ -1,27 +1,95 @@
-import React from 'react';
-import { MDBMask, MDBView, MDBRow, MDBContainer, MDBCol } from 'mdbreact';
-import ReactDOM from "react-dom";
+import React from "react";
+import { BrowserRouter as Router } from "react-router-dom";
+import { MDBNavbar, MDBNavbarBrand, MDBNavbarNav, MDBNavItem, MDBNavLink, MDBNavbarToggler, MDBCollapse, MDBMask,
+MDBRow, MDBCol, MDBBtn, MDBView, MDBContainer, MDBFormInline } from "mdbreact";
+import "./landing.css";
 
-class StartPage extends React.Component {
-  render() {
-    return (
-      <MDBContainer className="mt-5">
+class AppPage extends React.Component {
+state = {
+  collapsed: false
+};
+
+handleTogglerClick = () => {
+this.setState({
+  collapsed: !this.state.collapsed
+});
+};
+
+render() {
+const overlay = (
+  <div id="sidenav-overlay" style={{ backgroundColor: "transparent" }} onClick={this.handleTogglerClick} />
+);
+return (
+<div id="apppage">
+  <Router>
+    <div>
+      <MDBNavbar color="primary-color" dark expand="md" fixed="top" scrolling transparent>
+        <MDBContainer>
+          <MDBNavbarBrand>
+            <strong className="white-text">MDB</strong>
+          </MDBNavbarBrand>
+          <MDBNavbarToggler onClick={this.handleTogglerClick} />
+          <MDBCollapse isOpen={this.state.collapsed} navbar>
+            <MDBNavbarNav left>
+              <MDBNavItem active>
+                <MDBNavLink to="#!">Home</MDBNavLink>
+              </MDBNavItem>
+              <MDBNavItem>
+                <MDBNavLink to="#!">Link</MDBNavLink>
+              </MDBNavItem>
+              <MDBNavItem>
+                <MDBNavLink to="#!">Profile</MDBNavLink>
+              </MDBNavItem>
+            </MDBNavbarNav>
+            <MDBNavbarNav right>
+              <MDBNavItem>
+                <MDBFormInline waves>
+                  <div className="md-form my-0">
+                    <input className="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search" />
+                  </div>
+                </MDBFormInline>
+              </MDBNavItem>
+            </MDBNavbarNav>
+          </MDBCollapse>
+        </MDBContainer>
+      </MDBNavbar>
+      {this.state.collapsed && overlay}
+    </div>
+  </Router>
+  <MDBView>
+    <MDBMask className="d-flex justify-content-center align-items-center gradient">
+      <MDBContainer>
         <MDBRow>
-          <MDBCol md="20">
-			<MDBView>
-				<img src="https://sbhack19-prod.s3.eu-central-1.amazonaws.com/public-resources/landing.jpg"  className="img-fluid" alt="" />
-				  <MDBMask className="flex-center" overlay="teal-slight">
-					<p className="white-text">SYNAPSES</p>
-				  </MDBMask>
-			</MDBView>
-      
-          </MDBCol>
+          <div className="white-text text-center text-md-left col-md-6 mt-xl-5 mb-5">
+            <h1 className="h1-responsive font-weight-bold mt-sm-5">
+              SYNAPSES{" "}
+            </h1>
+            <hr className="hr-light" />
+            <h6 className="mb-4">
+              Universal marketplace to incentivize value creation
+            </h6>
+            <MDBBtn color="white">Download</MDBBtn>
+            <MDBBtn outline color="white">
+              Learn More
+            </MDBBtn>
+          </div>
         </MDBRow>
       </MDBContainer>
-    );
-  }
+    </MDBMask>
+  </MDBView>
+
+  <MDBContainer>
+    <MDBRow className="py-5">
+      <MDBCol md="12" className="text-center">
+        <p>
+          We use ETH smart-contracts to bring together anyone who wants to sell any type of asset (data, multimedia, expertise, physical entities) with whose who need these assets.
+        </p>
+      </MDBCol>
+    </MDBRow>
+  </MDBContainer>
+</div>
+);
+}
 }
 
-export default StartPage;
-
-
+export default AppPage;
