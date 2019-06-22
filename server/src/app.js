ecd6c0e28c9c57e2e4cffa041b649e41f07e971e
Janusz Tomasik <januszveiro@gmail.com>
2019-06-22 13:58:42 +0200

    Add metamask page

--- server/src/app.js
+++ server/src/app.js
@@ -1,11 +1,13 @@
 const express = require('express')
 require('./db/mongoose')
 const userRouter = require('./routers/user')
+const bodyParser = require('body-parser');
 
 const app = express()
 const port = process.env.PORT || 3000
 
 app.use(express.json())
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.all('/*', function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
