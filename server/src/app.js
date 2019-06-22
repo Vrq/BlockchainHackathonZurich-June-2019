29061e4dd5cb0490cc87d8fc52cf4f6fdee6f5fb
Ben Bullinger <47496939+EBNF@users.noreply.github.com>
2019-06-22 04:38:26 +0200

    Add U&D op's, refactor CRUD op's to model-specific router

--- server/src/app.js
+++ server/src/app.js
@@ -1,11 +1,12 @@
 const express = require('express')
 require('./db/mongoose')
-const User = require('./models/user')
+const userRouter = require('./routers/user')
 
 const app = express()
 const port = process.env.PORT || 3000
 
 app.use(express.json())
+app.use(userRouter)
 
 app.get('', (req, res) => {
     res.send('Hello express!')
@@ -13,42 +14,6 @@ app.get('', (req, res) => {
 
 app.get('/about', (req, res) => {
     res.send('About')
-})
-
-app.post('/users', async (req, res) => {
-    const user = new User(req.body)
-
-    try {
-        await user.save()
-        res.status(201).send(user)
-    } catch (e) {
-        res.status(400).send(e)
-    }
-})
-
-app.get('/users', async (req, res) => {
-    try {
-        const users = await User.find({})
-        res.send(users)
-    } catch (e) {
-        res.status(500).send()
-    }
-})
-
-app.get('/users/:id', async (req, res) => {
-    const _id = req.params.id
-
-    try {
-        const user = await User.findById(_id)
-
-        if (!user) {
-            return res.status(404).send()
-        }
-
-        res.send(user)
-    } catch (e) {
-        res.status(500).send()
-    }
 })
 
 app.listen(port, () => {
