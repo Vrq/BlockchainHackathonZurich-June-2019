47cdf84c95c22b29d434f573027a9c0d6f32acc1
Ben Bullinger <47496939+EBNF@users.noreply.github.com>
2019-06-23 09:31:00 +0200

    basic verified upload, hashing, hash storage (no duplicate naming)

--- server/src/models/user.js
+++ server/src/models/user.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose')
 const validator = require('validator')
 const bcrypt = require('bcryptjs')
 const jwt = require('jsonwebtoken')
+const File = require('./file')
 
 const userSchema = new mongoose.Schema({
     name: {
