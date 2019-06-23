3a86ce81e7bc19c4f3e3220e142f9aa82b99c1af
Ben Bullinger <47496939+EBNF@users.noreply.github.com>
2019-06-23 10:55:35 +0200

    Return hashData json on upload  % clean up unused file model

--- server/src/routers/file.js
+++ server/src/routers/file.js
@@ -46,13 +46,13 @@ router.post('/users/me/upload', [auth, upload.array('upl', 1)], (req, res, next)
     }
 
     // download uploaded file's buffer from s3
-    getObject('sbhack19-prod/upload', key).then( async (res) => {
+    getObject('sbhack19-prod/upload', key).then( async (res2) => {
 
         // hash upload file buffer
-        const hash = await bcrypt.hash(res, 8)
+        const hash = await bcrypt.hash(res2, 8)
 
         // create hash data json
-        data = {
+        const hashData = {
             filename: key.split('.').slice(0, -1).join('.') + '.json', //replace extension (taken from upload) with .json
             hash,
             owner: req.user._id
@@ -61,8 +61,8 @@ router.post('/users/me/upload', [auth, upload.array('upl', 1)], (req, res, next)
         // upload hash data json to s3
         s3.putObject({ 
             Bucket: 'sbhack19-prod/hashData', 
-            Key: timeStamp + '_' + data.filename, 
-            Body: JSON.stringify(data),
+            Key: timeStamp + '_' + hashData.filename, 
+            Body: JSON.stringify(hashData),
             ContentType: "application/json"
         }, function (err, data) {
             if (err) {
@@ -86,9 +86,11 @@ router.post('/users/me/upload', [auth, upload.array('upl', 1)], (req, res, next)
                 }).promise()
             )
             .catch((e) => console.error(e))
+
+        res.send(hashData)
     })
 
-    res.send("Uploaded file, personal hash created & stored!")
+    // res.send() ///?
 }, (error, req, res, next) => {
     res.status(400).send({ error: error.message })
 })
